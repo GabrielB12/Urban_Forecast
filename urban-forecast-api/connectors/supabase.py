0f029089ad8535040598e1a6437a25bdbee8a432
Gabriel Bianchi <gabriel.bianchi@unifesp.br>
2026-04-25 23:01:39 -0300

    fix previsao

--- urban-forecast-api/connectors/supabase.py
+++ urban-forecast-api/connectors/supabase.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def fetch_data(url, key, sensor_id):
-    endpoint = f"{url}/rest/v1/v_distancias_com_fill?select=*&sensor_id=eq.{sensor_id}&order=created_at.asc"
+    endpoint = f"{url}/rest/v1/v_distancias_com_fill?select=*&sensor_id=eq.{sensor_id}&order=created_at.desc&limit=100"
     res = requests.get(endpoint, headers={
         "apikey": key,
         "Authorization": f"Bearer {key}"
@@ -12,5 +12,5 @@ def fetch_data(url, key, sensor_id):
     if not data:
         return pd.DataFrame()
     df = pd.DataFrame(data)
-    df["created_at"] = pd.to_datetime(df["created_at"], utc=True)  # <-- utc=True
+    df["created_at"] = pd.to_datetime(df["created_at"], utc=True)
     return df
